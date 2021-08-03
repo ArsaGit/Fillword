a23b70075403df6d2905e534a6fb70ad3e042a15
l <l>
2021-08-03 16:49:41 +0500

    добавил прототип класса поля для слов. добавил словарь и класс для работы с файлами.

---
+++ Field.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fillword
+{
+	public class Field
+	{
+		private int fieldWidth;
+		private int fieldHeight;
+
+		char[,] field;
+
+		private class WordData
+		{
+			public string Word { get; set; }
+			public List<int[]> Coordinades { get; set; }
+		}
+
+		private List<WordData> words;
+
+		private const ConsoleColor CurrentCellColor = ConsoleColor.Green;
+		private const ConsoleColor SelectedCellColor = ConsoleColor.Blue;
+		private const ConsoleColor GuessedCellColor = ConsoleColor.Gray;
+
+		public Field()
+		{
+			fieldHeight = 6;
+			fieldWidth = 6;
+			GenerateField();
+		}
+
+		public void GenerateField()
+		{
+			field = new char[fieldHeight, fieldWidth];
+
+		}
+
+		private void GetWords()
+		{
+
+		}
+	}
+}
---
+++ WorkWithFIles.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Fillword
+{
+	public class WorkWithFIles
+	{
+		//создаёт относительный путь к фалйу
+		private string GetPathToFile(string fileName)
+		{
+			string filePath = Environment.CurrentDirectory;
+			filePath = filePath.Substring(0, filePath.IndexOf("bin")) + fileName;
+			return filePath;
+		}
+
+		//считывает слова из файла и возвращает лист строк
+		public List<string> ReadFile()
+		{
+			List<string> input = new List<string>();
+			string path = GetPathToFile("russian_nouns.txt");
+
+			foreach (string line in File.ReadLines(path))
+			{
+				input.Add(line);
+			}
+			return input;
+		}
+
+		//записывает слова в файл
+		public void WriteInFile(string content)
+		{
+			string path = GetPathToFile("russian_nouns.txt");
+			using (StreamWriter sw = new StreamWriter(path))
+			{
+				sw.WriteLine(content);  //запись данных
+			}
+		}
+	}
+}
